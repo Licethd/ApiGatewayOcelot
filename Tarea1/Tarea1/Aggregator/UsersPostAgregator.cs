4e44f86f7c3e4b7799e1712f35c1a73cbaec84c3
Licethd <liceth.dfe@gmail.com>
2022-09-17 21:54:56 -0400

    tarea1

---
+++ Tarea1/Tarea1/Aggregator/UsersPostAgregator.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.Net.Http.Headers;
+using tarea1.Dto;
+using Newtonsoft.Json;
+using Ocelot.Middleware;
+using Ocelot.Multiplexer;
+
+
+namespace tarea1.Aggregator
+{
+    public class ExampleAgregator : IDefinedAggregator
+    {
+        public async Task<DownstreamResponse> Aggregate(List<HttpContext> responses)
+        {
+            if (responses.Any(x => x.Items.Errors().Count > 0))
+            {
+                return new DownstreamResponse(null, HttpStatusCode.BadRequest, null as List<Header>, null);
+            }
+            //LLEGADA DE DATOS
+            var userDatas = await responses[0].Items.DownstreamResponse().Content.ReadAsStringAsync();
+            var postDatas = await responses[1].Items.DownstreamResponse().Content.ReadAsStringAsync();
+
+            var usuarios = JsonConvert.DeserializeObject<List<User>?>(userDatas);
+            var posts = JsonConvert.DeserializeObject<List<Post>?>(postDatas);
+
+
+            //VERIFICANDO COINCIDENCIAS
+            foreach (var user in usuarios!)
+            {
+                var result = posts?.Where(p => p.UserId == user.Id).ToList();
+                user.Posts?.AddRange(result!);
+            }
+
+            var postByUserString = JsonConvert.SerializeObject(usuarios);
+            var stringContent = new StringContent(postByUserString)
+
+            {
+                Headers = { ContentType = new MediaTypeHeaderValue("application/json") }
+            };
+
+            return new DownstreamResponse(stringContent, HttpStatusCode.OK, new List<KeyValuePair<string, IEnumerable<string>>>(), "OK");
+
+        }
+    }
+}
